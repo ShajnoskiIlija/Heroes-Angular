cb4364939144d31a16c8c22bc65c6ceba5ad4548
ShajnoskiIlija <shajnoskiilija@gmail.com>
2022-06-01 14:31:51 +0200

    Displaying List of Heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 10, name: 'Dr. Nice' },
+  { id: 11, name: 'Bombasto' },
+  { id: 12, name: 'Celeritas' },
+  { id: 13, name: 'Magneta' },
+  { id: 14, name: 'RubberMan' },
+  { id: 15, name: 'Dynama' },
+  { id: 16, name: 'Dr. IQ' },
+  { id: 17, name: 'Magma' },
+  { id: 18, name: 'Tornado' }
+];
